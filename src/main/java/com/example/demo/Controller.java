733ab3fae13d3fba7361f77e4bb8de335a783272
gfmota <gabrielfmota134@gmail.com>
2024-05-16 16:52:31 -0300

    http interfaces
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
@@ -14,15 +17,20 @@ import java.util.List;
 @RestController("/")
 public class Controller {
 
-    private WebClient webClient = WebClient.builder().baseUrl("https://pokeapi.co/api/v2").build();
+    private PokeApiClient pokeApiClient;
 
-//    @Autowired
-//    private PokeApiClient pokeApiClient;
+    public Controller() {
+        WebClient webClient = WebClient.builder().baseUrl("https://pokeapi.co/api/v2/").clientConnector().build();
+        WebClientAdapter adapter = WebClientAdapter.create(webClient);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
 
-    @GetMapping("/")
-    public ResponseEntity<?> getPokemonList() {
+        this.pokeApiClient = factory.createClient(PokeApiClient.class);
+    }
+
+    @GetMapping("/{from}/{to}")
+    public ResponseEntity<?> getPokemonList(@PathVariable int from, @PathVariable int to) {
         List<Flux<Pokemon>> requests = new ArrayList<>();
-        for (int i = 1; i <= 9; i++) {
+        for (int i = from; i <= to; i++) {
             requests.add(requestPokemon(i));
         }
 //        requestPokemon(2).subscribe(res -> {
@@ -45,10 +53,7 @@ public class Controller {
 
     private Flux<Pokemon> requestPokemon(final int id){
         try {
-            return webClient.get()
-                    .uri("/pokemon/" + id)
-                    .retrieve()
-                    .bodyToFlux(Pokemon.class);
+            return pokeApiClient.getPokemonList(id);
         } catch (WebClientResponseException e) {
             return Flux.just(new Pokemon());
         }
--- src/main/java/com/example/demo/PokeApiClient.java
+++ src/main/java/com/example/demo/PokeApiClient.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import reactor.core.publisher.Flux;
+
 import java.util.List;
 
 public interface PokeApiClient {
 
-    public List<Pokemon> getPokemonList();
+    @GetExchange("pokemon/{id}")
+    public Flux<Pokemon> getPokemonList(@PathVariable int id);
 }
